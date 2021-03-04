23cdb2a1f0aee411d42c56d42da0af525bb5fcaf
Ali Mert Özhayta <mozhayta@hotmail.com>
2021-03-04 22:51:51 +0300

    Initial Commit

---
+++ HMS ML Kit - Translation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HMS ML Kit - Translation
+//
+//  Created by Ali Mert Özhayta on 4.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
